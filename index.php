f54d3e773116da48565a902204934e508115da86
Ozaki Yoichi (zak) <zak@inl.ics.keio.ac.jp>
2022-11-07 18:11:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 $NS=array('8.8.8.8');
-$dns_NS_array=dns_get_record("www.google.com", DNS_NS, $NS);
+$dns_NS_array=dns_get_record("www.google.com", DNS_ANY, $NS);
 print_r($dns_NS_array);
 
 $NS=array('10.0.1.4');
-$dns_NS_array=dns_get_record("private.yoichiozaki.com", DNS_NS, $NS);
+$dns_NS_array=dns_get_record("private.yoichiozaki.com", DNS_ANY, $NS);
 print_r($dns_NS_array);
 
 $NS=array('8.8.8.8');
-$dns_NS_array=dns_get_record("www.yoichiozaki.com", DNS_NS, $NS);
+$dns_NS_array=dns_get_record("www.yoichiozaki.com", DNS_ANY, $NS);
 print_r($dns_NS_array);
